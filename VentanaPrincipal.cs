32edfd768753108a360b8dd9fa7b345786f2406d
xp <xp@CETYSDAM00>
2022-05-09 19:01:18 +0200

    Agregar archivos de proyecto.

---
+++ VentanaPrincipal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veterinario
+{
+    public partial class VentanaPrincipal : Form
+    {
+        Conexion conexion = new Conexion();
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxDNI_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertaUsuario_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(conexion.InsertaUsuario(textBoxDNI.Text, textBoxNombre.Text, textBoxPass.Text));
+        }
+    }
+}
